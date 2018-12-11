37275a2be668a2053c210e7ceae11902966d205d
github username <github email address>
2018-12-11 17:29:48 -0500

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,20 @@
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function domContent(){
   const taskList = new TaskList();
-});
+  const taskForm =  document.getElementById('create-task-form')
+    console.log(taskForm)
+    taskForm.addEventListener('submit', function formSubmitEventHandler(event) {
+      event.preventDefault()
+        console.log(event.target)
+      const userInputField = event.target.querySelector('#new-task-description')
+       console.log(userInputField)
+      const userInputString = userInputField.value
+        console.log(userInputString)
+      const commentPTag = document.createElement('li')
+        console.log(commentPTag)
+        commentPTag.textContent = userInputString
+       const list = document.querySelector('#list') 
+        list.appendChild(commentPTag)  
+    })
+  
+  })
